6ba6f6858d7d3592d5c18830b24923429427b1ca
Hemant242 <hemant.kunder30@gmail.com>
2021-08-10 20:48:25 +0530

    login page added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import './App.scss';
 import Home from './page/home/Home';
+import Login from './page/login/Login.jsx';
 import Register from './page/register/Register';
 import Watch from './page/watch/Watch';
 
 const App = () => {
   return (
     <div className="App">
-    <Home />
+   <Register/>
      
     </div>
   );
